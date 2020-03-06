011f0d89e6e22f68d540adaeb786e13e00059a4a
hwseward <haydenseward78@gmail.com>
2020-03-06 00:18:30 -0800

    crates

--- file_tranfer/src/bin/host_main.rs
+++ file_tranfer/src/bin/host_main.rs
@@ -1,9 +1,13 @@
 use file_transfer::ftp::*;
 use std::fs::File;
 
+
+/// Example
 fn main() -> std::io::Result<()> {
 
+    // Creates new FTP
     let ftp = FTP::new("127.0.0.1:8080", File::create("/home/denard/Pictures/Test.png").unwrap());
+    // Receives File
     ftp.recv();
     Ok(())
 
